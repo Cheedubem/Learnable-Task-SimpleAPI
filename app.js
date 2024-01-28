423d573df58ce78a658724436b701802a2ed9d51
Cheedubem <adimonyechidubem@gmail.com>
2024-01-28 09:11:55 +0100

    fixed nonsense bug

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// this is a constructor for the movie class
+// this is a constructor for the anime class
 
 function anime(title, genre, year, studio, episodes) {
   this.title = title;
@@ -8,12 +8,12 @@ function anime(title, genre, year, studio, episodes) {
   this.studio = studio;
   this.isAvailable = true;
 
-  // this method gets movie information
+  // this method gets anime information
   this.getAnimeInfo = function () {
     return `${this.title} (${this.year}), Genre: ${this.genre}, Studio: ${this.studio}, Episodes: ${this.episodes}`;
   };
 
-  // this method rents the movie
+  // this method rents the anime
   this.rentAnime = function () {
     if (this.isAvailable) {
       this.isAvailable = false;
@@ -24,27 +24,20 @@ function anime(title, genre, year, studio, episodes) {
   };
 }
 
-// Creating some movie objects
-const anime1 = new anime("Berserk", "Seinen", 2023, "GEMBA", 75);
-const anime2 = new anime("Naruto", "Shounen", 2000, "Unknown", 500);
-const anime3 = new anime("Bleach", "Shounen", 2001, "Unknown", 375);
-const anime4 = new anime("Orange", "Romance", 2015, "Unknown", 25);
-
-// I need to store toh movies in an array to loop through it
+// I need to store toh anime in an array to loop through it
 const storedAnimes = [
-    anime1,
-    anime2,
-    anime3,
-    anime4;
+  new anime("Berserk", "Seinen", 2023, "GEMBA", 75),
+  new anime("Naruto", "Shounen", 2000, "Unknown", 500),
+  new anime("Bleach", "Shounen", 2001, "Unknown", 375),
+  new anime("Orange", "Romance", 2015, "Unknown", 25),
 ];
 
 // to get the animes by genre
-function selectAnimesByGenre(genre){
-    const selectedAnime = storedAnimes.filter(anime => anime.genre === genre)
-        return selectedAnime;
+function selectAnimesByGenre(genre) {
+  return storedAnimes.filter((anime) => anime.genre === genre);
 }
 
 // Choose Genre and Display Genre animes romance, just switch genre names
-const romance = selectAnimesByGenre("romance");
-console.log("Romance Animes: ");
-romance.forEach(movie => console.log(anime.getAnimeInfo()));
+const romance = selectAnimesByGenre("Romance");
+console.log("Romance Animes:");
+romance.forEach((anime) => console.log(anime.getAnimeInfo()));
